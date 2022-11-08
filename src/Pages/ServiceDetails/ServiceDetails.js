18bcd5ba5fe8b93c3958d8490d5ac37d87c0362f
maruf <dev.maruf112@gmail.com>
2022-11-09 01:17:49 +0600

    review shown on ui

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -4,18 +4,47 @@ import { useLoaderData } from "react-router-dom";
 const ServiceDetails = () => {
   const details = useLoaderData();
   console.log(details);
-  const { tour_name, img, desc, review } = details;
+  const { tour_name, img, desc, review } = details[0];
+  console.log(review);
+
   return (
-    <div className="card lg:card-side bg-base-100 shadow-xl">
-      <figure>
-        <img src="https://placeimg.com/400/400/arch" alt="Album" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">New album is released!</h2>
-        <p>Click the button to listen on Spotiwhy app.</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Listen</button>
+    <div>
+      <div className="max-w-6xl mx-auto">
+        <h2 className="text-center my-8 text-4xl font-bold underline">
+          Tour Package{" "}
+        </h2>
+        <div className="card lg:card-side bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">{tour_name}</h2>
+            <p>{desc}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Book Now</button>
+            </div>
+          </div>
         </div>
+      </div>
+      <h2 className="text-center my-8 text-4xl font-bold underline">
+        Travelers Reviews{" "}
+      </h2>
+      <div className="grid grid-cols-3 max-w-6xl mx-auto gap-10">
+        {review.map((rev) => (
+          <div className="max-w-6xl mx-auto">
+            <div className="card w-96 h-72 bg-base-100 shadow-xl">
+              <div className="card-body mx-auto">
+                <div className="avatar">
+                  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 mx-auto">
+                    <img src={rev.img} alt="user" />
+                  </div>
+                </div>
+                <div className="mt-5">
+                  {" "}
+                  <h2 className="card-title">{rev.name}</h2>
+                  <p className="mx-auto">{rev.review_text}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
