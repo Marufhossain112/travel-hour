e604afd0b1b12ac46345bb708c1bc1400d6f1dbc
maruf <dev.maruf112@gmail.com>
2022-11-08 07:12:11 +0600

    footer added from daisyUi

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
         <li><Link>Item 3</Link></li>
       </ul>
     </div>
-    <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+    <Link className="btn btn-ghost normal-case text-xl">Travel Hour</Link>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal p-0">
