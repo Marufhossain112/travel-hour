735bba64ca4faab92c9c3a006e59d7ab73fae5ec
maruf <dev.maruf112@gmail.com>
2022-11-10 02:03:16 +0600

    Show home added services

--- src/Pages/AddServiceCard/AddServiceCard.js
+++ src/Pages/AddServiceCard/AddServiceCard.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const AddServiceCard = ({ services }) => {
-  const { tour_name, img, price, desc } = services;
+  //   console.log(services);
+  const { tour_name, img, price, desc, _id } = services;
+  const handleAddService = (services) => {
+    // console.log(`I am adding service ${services}`);
+    console.log(services);
+
+    fetch("http://localhost:5000/servicesLimit", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(services),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
-        <img style={{height:"256px",width:"385px"}} src={img} alt="Shoes" />
+        <img
+          style={{ height: "256px", width: "385px" }}
+          src={img}
+          alt="travel"
+        />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{tour_name}</h2>
@@ -17,9 +35,12 @@ const AddServiceCard = ({ services }) => {
         </div>
         <div className="card-actions justify-center">
           {/* <button>View Details</button> */}
-          <Link to={`/services/${services._id}`}>
-            <button className="btn btn-primary">Add Service</button>
-          </Link>
+          <button
+            onClick={() => handleAddService(services)}
+            className="btn btn-primary"
+          >
+            Add Service
+          </button>
         </div>
       </div>
     </div>
