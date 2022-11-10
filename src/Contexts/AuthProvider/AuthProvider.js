78214059562832c5521af3d56df6da151d6b968a
maruf <dev.maruf112@gmail.com>
2022-11-10 11:16:57 +0600

    spinner added

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -12,17 +12,22 @@ export const MyContext = createContext();
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const signUpUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const signIn = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
   const googleSignIn = (provider) => {
+    setLoading(true);
     return signInWithPopup(auth, provider);
   };
 
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
 
@@ -30,12 +35,14 @@ const AuthProvider = ({ children }) => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       // console.log("current User inside state change", currentUser);
       setUser(currentUser);
+      setLoading(false);
     });
 
     return () => unSubscribe();
   }, []);
   const authInfo = {
     user,
+    loading,
     signUpUser,
     signIn,
     logOut,
--- src/PrivateRoute/PrivateRoute.js
+++ src/PrivateRoute/PrivateRoute.js
@@ -1,15 +1,20 @@
-
 import React from "react";
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { MyContext } from "../Contexts/AuthProvider/AuthProvider";
 
-
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(MyContext);
+  const { user, loading } = useContext(MyContext);
   const location = useLocation();
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-96">
+        <button className="btn btn-primary btn-circle loading"></button>
+      </div>
+    );
+  }
 
-// if there is no user
+  // if there is no user
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
   }
